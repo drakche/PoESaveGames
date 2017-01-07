b9bc9dcecc9aff97f588f1b118bbb7d32ec1683b
Dragan Stojanovic <stojanovic.dragan@live.com>
2017-01-07 22:41:14 +0100

    sync savegames

--- Pillars Sync Savegames.sh
+++ Pillars Sync Savegames.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd ~/.local/share/PillarsOfEternity/
-(((git add . && git commit -am "sync savegames" && git push) || git push) && /usr/bin/notify-send "Pillars of Ethernety synced save games!")
+cd ~/.local/share/PillarsOfEternity/ && (((git add . && git commit -am "sync savegames" && git push) || git push) && /usr/bin/notify-send "Pillars of Ethernety synced save games!")
